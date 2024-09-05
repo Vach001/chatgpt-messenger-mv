8cd8cdcc188d81c2dca096023582e0823273c100
Vache Apinyan <apinyan.v@gmail.com>
2024-09-05 18:21:17 +0400

    feat(*): add heroIcons, fix bugs and add sun icon to section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import { SunIcon } from '@heroicons/react/24/outline'
+
 function HomePage() {
   return (
-    <div className="text-white">
+    <div className=" flex flex-col items-center justify-center h-screen text-white">
         <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+        
+        <div>
+          <div>
+            <div className="flex flex-col items-center justify-center">
+              <SunIcon className="h-8 w-8" />
+              <h2>Examples</h2>
+            </div>
+
+            <div className="space-y-2">
+              <p className="infoText">"Explain Something to me"</p>
+              <p className="infoText">"What is the difference between a dog and a cat"</p>
+              <p className="infoText">"What is the color of the sun"</p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
