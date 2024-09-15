26c82b25e2aa09a88626a9e0d0d50b9327382511
Vache Apinyan <apinyan.v@gmail.com>
2024-09-15 16:24:25 +0400

    feat(app/layout.tsx): add session for login and add environment configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import SideBar from "@/components/SideBar";
 import "../styles/globals.css";
 import { SessionProvider } from "@/components/SessionProvider";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/pages/api/auth/[...nextAuth]";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import Login from "@/components/Login";
 
 export const metadata = {
   title: 'ChatGPT',
@@ -18,14 +19,19 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head />      
       <body>
-        <SessionProvider session ={session}> 
+        <SessionProvider session ={session}>
+          {!session ? (
+            <Login />
+          ) : (
           <div className="flex">
             <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
               <SideBar />
             </div>
             <div className="bg-[#343541] flex-1">{children}</div>
           </div>
+          )}          
         </SessionProvider>
       </body>
     </html>
