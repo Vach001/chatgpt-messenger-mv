e58105d93608450436213c7b3e67478b8d570c26
Vache Apinyan <apinyan.v@gmail.com>
2024-09-18 20:14:41 +0400

    feat(next.config.mjs): remove alredy enabled appDir flag

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['www.google.com', 'freepnglogo.com']
   },
   experimental: {  
-    appDir: true
   }
 };
 
