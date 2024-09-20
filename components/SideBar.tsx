e876c6d57c61f97f9c9d9b009704fa64dd66b2ba
Vache Apinyan <apinyan.v@gmail.com>
2024-09-20 16:54:33 +0400

    feat(components/SideBar.tsx): add session user picture, styles and signOut function

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,17 +1,30 @@
+'use client'
+
+import { useSession, signOut } from "next-auth/react"
 import NewChat from "./NewChat"
 
 function SideBar() {
+    const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
-        <div className="flex-1 ">
-           <NewChat />
-            <div>
-                {/* modules */}
-            </div>
-            {/* chatRows */}
+      <div className="flex-1 ">
+        <div>
+          <NewChat />
+          <div> {/* ModelSellection */} </div>
+          {/* ChatRows */}
         </div>
+      </div>
+      {session && (
+        <img 
+        onClick={()=>signOut()}
+        src={session.user?.image!} 
+        alt="No profile picture" 
+        className="h-12 w-12 rounded-full cursor-pointer max-auto mb-2 hover:opacity-50"
+        />
+      )} 
     </div>
-  )
+  );
 }
 
 export default SideBar
