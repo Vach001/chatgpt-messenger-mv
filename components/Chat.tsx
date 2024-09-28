8e591c107bac5293dcc2a0c73c7e201792d50ae9
Vache Apinyan <apinyan.v@gmail.com>
2024-09-28 16:28:46 +0400

    feat(components/chat.tsx): creat Chat chomponent

---
+++ components/Chat.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+type Props = {
+    chatId: string;
+}
+
+function Chat({ chatId }: Props) {
+  return (
+    <div className="flex-1">
+        Chat
+    </div>
+  )
+}
+
+export default Chat
