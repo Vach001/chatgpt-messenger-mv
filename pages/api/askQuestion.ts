f9ce202c9cf9308ea468a846b306a24ca684c908
Vache Apinyan <apinyan.v@gmail.com>
2024-10-10 14:55:40 +0400

    feat(pages/api/askQuestion.ts): building ask question query

---
+++ pages/api/askQuestion.ts
@@ -0,0 +1,51 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import query from "../../lib/queryApi";
+import type { NextApiRequest, NextApiResponse } from "next";
+import admin from "firebase-admin"
+import { adminDB } from "@/firebaseAdmin";
+
+type Data = {
+  answer: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) 
+{
+  const { prompt, chatId, model, session } = req.body;
+  
+  if(!prompt) {
+    res.status(400).json({ answer: "Please provide a prompt!"});
+    return;
+  }
+
+  if(!chatId) {
+    res.status(400).json({ answer: "Please provide a valid chat ID!"});
+    return;
+  }
+  
+  //ChatGPT Query
+  
+  const response = await query(prompt, chatId, model)
+
+  const message: Message = {
+    text: response || "ChatGPT was unable to find an answer for that!",
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: "ChatGPT",
+      name: "ChatGPT",
+      avatar: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/chatgpt-icon.png"
+    },
+  };
+
+  await adminDB
+  .collection("user")
+  .doc(session?.user?.email)
+  .collection("chats")
+  .doc(chatId)
+  .collection("messages")
+  .add(message);
+
+  res.status(200).json({ answer: message.text });
+}
