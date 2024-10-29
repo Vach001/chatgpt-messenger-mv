b759d102fb59f2069c28d43a4b8ab8e90b13c8d4
Vache Apinyan <apinyan.v@gmail.com>
2024-10-29 16:00:32 +0400

    feat(components/Message.tsx): correcting readability

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,21 +1,24 @@
-import { DocumentData } from "firebase/firestore"
-
+import { DocumentData } from "firebase/firestore";
 
 type Props = {
-    message: DocumentData
-}
+  message: DocumentData;
+};
 
 function Message({ message }: Props) {
-    const isChatGPT = message.user.name === "ChatGPT";
-   
+  const isChatGPT = message.user.name === "ChatGPT";
+
   return (
     <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>
-        <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
-            <img src={message.user.avatar} alt="X" className="h-8 w-8 rounded-full" />
-            <p className="pt-1 text-sm"> {message.text} </p>
-        </div>
+      <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
+        <img
+          src={message.user.avatar}
+          alt="X"
+          className="h-8 w-8 rounded-full"
+        />
+        <p className="pt-1 text-sm"> {message.text} </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Message
+export default Message;
