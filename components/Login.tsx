e00002c76717852b4c3f978bedf2f7f0cdaef272
Vache Apinyan <apinyan.v@gmail.com>
2024-10-29 15:59:25 +0400

    feat(components/Login.tsx): correcting readability

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,22 +1,25 @@
-'use client'
+"use client";
 
-import { signIn } from "next-auth/react"
-import Image from "next/image"
+import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 function Login() {
   return (
     <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
-        <Image
+      <Image
         src="https://freepnglogo.com/images/all_img/1724866361chatgpt-logo-png-green-background.png"
         width={300}
         height={300}
-        alt="logo" />
-        <button onClick={() => signIn('google')}
-        className="text-white font-bold text-3xl animate-pulse"> 
-            Sign In to use ChatGPT
-        </button>
+        alt="logo"
+      />
+      <button
+        onClick={() => signIn("google")}
+        className="text-white font-bold text-3xl animate-pulse"
+      >
+        Sign In to use ChatGPT
+      </button>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
