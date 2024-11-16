715bbea38ca53543e60e23474419a2deb91e60f2
Vache Apinyan <apinyan.v@gmail.com>
2024-11-16 18:32:09 +0400

    feat(components/Chat.tsx): correcting format Chat component

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { db } from "@/firebase";
 import { collection, orderBy, query } from "firebase/firestore";
@@ -7,35 +7,40 @@ import { useCollection } from "react-firebase-hooks/firestore";
 import Message from "./Message";
 
 type Props = {
-    chatId: string;
-}
+  chatId: string;
+};
 
 function Chat({ chatId }: Props) {
   const { data: session } = useSession();
 
   const [messages] = useCollection(
-    session && 
-    query(
-      collection(db, "users", session?.user?.email!, "chats", chatId, "messages"), 
-      orderBy("createdAt", "asc")
-    )
+    session &&
+      query(
+        collection(
+          db,
+          "users",
+          session?.user?.email!,
+          "chats",
+          chatId,
+          "messages"
+        ),
+        orderBy("createdAt", "asc")
+      )
   );
 
   return (
     <div className="flex-1 overflow-y-auto overflow-x-hidden">
       {messages?.empty && (
         <>
-        <p >
-
-        </p>
+          <p></p>
         </>
       )}
-      
-      { messages?.docs.map((message) => (
+
+      {messages?.docs.map((message) => (
         <Message key={message.id} message={message.data()} />
       ))}
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
