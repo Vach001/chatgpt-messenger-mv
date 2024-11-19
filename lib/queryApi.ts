d6476f871e107b3e3cd460de346ead59a4299068
Vache Apinyan <apinyan.v@gmail.com>
2024-11-19 21:43:10 +0400

    feat(lib/queryApi.ts): rewrite query async function

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -1,21 +1,28 @@
-import openai from "./chatgpt";
+import { OpenAI } from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY!,
+});
 
 const query = async (prompt: string, chatId: string, model: string) => {
-  try {
-    const response = await openai.completions.create({
+  openai.completions
+    .create({
       model,
       prompt,
       temperature: 0.9,
       top_p: 1,
       max_tokens: 1000,
       frequency_penalty: 0,
-    });
-
-    return response.choices[0]?.text?.trim();
-  } catch (err: any) {
-    console.error(`Error fetching response from OpenAI: ${err.message}`);
-    return `ChatGPT was unable to find an answer for that! (Error: ${err.message})`;
-  }
+      presence_penalty: 0,
+    })
+    .then((response) => {
+      return response.choices[0]?.text || null;
+    })
+    .catch(
+      (error) => 
+        `ChatGPT was unable to find an answer for that! (Error: ${error.message})`
+    );
+  return null;
 };
 
 export default query;
