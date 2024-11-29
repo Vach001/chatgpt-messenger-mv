b6adcdb5fadfb4f9fb92d416bae5087ab6a1d0e1
Vache Apinyan <apinyan.v@gmail.com>
2024-11-29 13:50:17 +0400

    fix(pages/api/askQuestion.ts): fixing bug to db

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import query from "../../lib/queryApi";
 import type { NextApiRequest, NextApiResponse } from "next";
-import admin from "firebase-admin"
+import admin from "firebase-admin";
 import { adminDB } from "@/firebaseAdmin";
 
 type Data = {
@@ -10,42 +10,46 @@ type Data = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) 
-{
+  res: NextApiResponse<Data>
+) {
   const { prompt, chatId, model, session } = req.body;
-  
-  if(!prompt) {
-    res.status(400).json({ answer: "Please provide a prompt!"});
+
+  if (!prompt) {
+    res.status(400).json({ answer: "Please provide a prompt!" });
     return;
   }
 
-  if(!chatId) {
-    res.status(400).json({ answer: "Please provide a valid chat ID!"});
+  if (!chatId) {
+    res.status(400).json({ answer: "Please provide a valid chat ID!" });
     return;
   }
-  
+
   //ChatGPT Query
-  
-  const response = await query(prompt, chatId, model)
+  try {
+    const response = await query(prompt, chatId, model);
 
-  const message: Message = {
-    text: response || "ChatGPT was unable to find an answer for that!",
-    createdAt: admin.firestore.Timestamp.now(),
-    user: {
-      _id: "ChatGPT",
-      name: "ChatGPT",
-      avatar: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/chatgpt-icon.png"
-    },
-  };
+    const message: Message = {
+      text: response || "ChatGPT was unable to find an answer for that!",
+      createdAt: admin.firestore.Timestamp.now(),
+      user: {
+        _id: "ChatGPT",
+        name: "ChatGPT",
+        avatar:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/chatgpt-icon.png",
+      },
+    };
 
-  await adminDB
-  .collection("user")
-  .doc(session?.user?.email)
-  .collection("chats")
-  .doc(chatId)
-  .collection("messages")
-  .add(message);
+    await adminDB
+      .collection("user")
+      .doc(session?.user?.email)
+      .collection("chats")
+      .doc(chatId)
+      .collection("messages")
+      .add(message);
 
-  res.status(200).json({ answer: message.text });
+    res.status(200).json({ answer: message.text });
+  } catch (error) {
+    console.error("Error in handler:", error);
+    res.status(500).json({ answer: "Internal server error" });
+  }
 }
