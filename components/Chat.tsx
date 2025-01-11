a8ef048d85257ac840017f0c350d438da413ccee
Vache Apinyan <apinyan.v@gmail.com>
2025-01-11 19:57:09 +0400

    feat(components/Chat.tsx): add hint to typemessage

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -5,6 +5,7 @@ import { collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import Message from "./Message";
+import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 
 type Props = {
   chatId: string;
@@ -32,7 +33,10 @@ function Chat({ chatId }: Props) {
     <div className="flex-1 overflow-y-auto overflow-x-hidden">
       {messages?.empty && (
         <>
-          <p> TEST</p>
+          <p className="mt-10 text-center text-white">
+            Type a prompt in below to get started!
+          </p>
+          <ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 text-whitw animate-bounce" />
         </>
       )}
       
